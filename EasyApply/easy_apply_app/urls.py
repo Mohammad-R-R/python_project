80f587da544234bb3002adef18e715a48c0bb0a2
Mahmoud Anbousi <anbousi@gmail.com>
2021-06-02 15:52:37 +0300

    added html pages and routes

--- EasyApply/easy_apply_app/urls.py
+++ EasyApply/easy_apply_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
-
-
-
 urlpatterns = [
-    path('',views.home)
+    path('',views.home),
+    path('login' , views.login),
+    path('register',views.register),
+    path('user/<int:id>',views.user)
+
 ]
--- EasyApply/easy_apply_app/views.py
+++ EasyApply/easy_apply_app/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(requset):
-    
     return render(requset, "home.html")
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'registration.html')
+
+def user(request, id):
+    return render(request, 'user.html')
+

bfbbc554d8f87fe6266031a483112abafe173bce
Mahmoud Anbousi <anbousi@gmail.com>
2021-06-05 09:18:07 +0300

    new version files

---
+++ Easy/easy_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home),
+    path('login' , views.login),
+    path('registration',views.registration),
+    path('reg',views.reg),
+    path('log',views.log),
+    path('user/<int:id>',views.user),
+    path('test',views.cv_test),
+    path('test2',views.CV_test),
+    path('showjob',views.showjob),
+    path('logout/',views.logout)
+]
--- EasyApply/easy_apply_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EasyApplyAppConfig(AppConfig):
-    name = 'easy_apply_app'
